23f9c2f64f8f478f81339bccd5eb0282cfba6a85
epittab <ep7m@me.com>
2020-09-21 20:59:08 -0400

    added char implementation in switch statement

--- Switch/Switch.java
+++ Switch/Switch.java
@@ -10,8 +10,15 @@ public class Switch {
         System.out.println(checkLang("Bonjour"));
         System.out.println(checkLang("Hola"));
         System.out.println(checkLang("Ciao"));
+        checkLetter('A');
+        checkLetter('C');
+        checkLetter('D');
+        checkLetter('H');
     }
     public static String checkLang(String greeting) {
+        /* 
+            if there were not returns, then it would use break
+        */
         switch (greeting) {
             case "Hello":
                 return "English";
@@ -20,7 +27,30 @@ public class Switch {
             case "Hola":
                 return "Spanish";
             default:
-                return "Unidentified";
+                return "Unidentified Language";
+        }
+    }
+
+    public static void checkLetter(char letter){
+        switch (letter){
+            case 'A':
+                System.out.println("Found A");
+                break;
+            case 'B':
+                System.out.println("Found B");
+                break;
+            case 'C':
+                System.out.println("Found C");
+                break;
+            case 'D':
+                System.out.println("Found D");
+                break;
+            case 'E':
+                System.out.println("Found E");
+                break;
+            default:
+                System.out.println("Could not find the letter");
+                break;
         }
     }
 }

6fdf091b75f8e24ea73ec007feb7724dda0ada94
epittab <ep7m@me.com>
2020-10-20 19:49:05 -0400

    updated code

--- For/For.java
+++ For/For.java
@@ -3,7 +3,7 @@ package For;
 public class For {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println(i+1);
+            System.out.println(i+2);
         }
     }
 }
--- While/While.java
+++ While/While.java
@@ -4,7 +4,7 @@ public class While {
     public static void main(String[] args){
         int i = 0;
         while (i < 10) {
-            System.out.println(i+1);
+            System.out.println(i+2);
             i++;
         }
     }
